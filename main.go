2d6bb820542a1564dd3ae607390ae48793d0bd48
Guilherme Lopes <guilhermebidarra@gmail.com>
2021-03-15 13:51:11 +0000

    most instructions

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 var memory [math.MaxInt16]uint16
 
+//TODO: falta operando RET
 func main() {
 
 	if len(os.Args) < 2 {
@@ -51,7 +52,7 @@ func main() {
 			imm_flag := (instr >> 5) & 0x1
 
 			if imm_flag == 1 {
-				imm5 := sign_extend(instr&0x1F, 5)
+				imm5 := signExtend(instr&0x1F, 5)
 				regs.REG[r0] = regs.REG[r1] + imm5
 
 			} else {
@@ -59,36 +60,102 @@ func main() {
 				regs.REG[r0] = regs.REG[r1] + regs.REG[r2]
 			}
 
-			update_flags(r0)
+			updateFlags(r0)
 			break
 		case OPS.AND:
+
+			r0 := (instr >> 9) & 0x7
+			r1 := (instr >> 6) & 0x7
+			imm_flag := (instr >> 5) & 0x1
+
+			if imm_flag == 1 {
+				imm5 := signExtend(instr&0x1F, 5)
+				regs.REG[r0] = regs.REG[r1] & imm5
+
+			} else {
+				var r2 uint16 = instr & 0x7
+				regs.REG[r0] = regs.REG[r1] & regs.REG[r2]
+			}
+
+			updateFlags(r0)
+
 			break
 		case OPS.NOT:
+			r0 := (instr >> 9) & 0x7
+			r1 := (instr >> 6) & 0x7
+			regs.REG[r0] = ^regs.REG[r1]
 			break
 		case OPS.BR:
+			n := (instr >> 9) & 0x1
+			z := (instr >> 10) & 0x1
+			p := (instr >> 11) & 0x1
+			PCoffset9 := signExtend(instr&0x1FF, 9)
+
+			if (n&fls.FL_NEG) == 1 || (z&fls.FL_ZRO) == 1 || (p&fls.FL_POS) == 1 {
+				regs.REG[regs.R_PC] = regs.REG[regs.R_PC] + PCoffset9
+			}
 			break
 		case OPS.JMP:
+			r1 := (instr >> 6) & 0x7
+			regs.REG[regs.R_PC] = regs.REG[r1]
 			break
 		case OPS.JSR:
+			regs.REG[regs.R_R7] = regs.REG[regs.R_PC]
+			mode := (instr >> 11) & 0x1
+			if mode == 0 {
+				r1 := (instr >> 6) & 0x7
+				regs.REG[regs.R_PC] = regs.REG[r1]
+			} else {
+				PCoffset11 := signExtend(instr&0x7FF, 11)
+				regs.REG[regs.R_PC] = regs.REG[regs.R_PC] + PCoffset11
+			}
 			break
 		case OPS.LD:
+			r0 := (instr >> 9) & 0x7
+			PCoffset9 := signExtend(instr&0x1FF, 9)
+			regs.REG[r0] = mem_read(regs.REG[regs.R_PC] + PCoffset9)
+			updateFlags(r0)
+
 			break
 		case OPS.LDI:
 			r0 := (instr >> 9) & 0x7
-			pc_offset := sign_extend(instr&0x1FF, 9)
-
-			regs.REG[r0] = mem_read(mem_read(regs.REG[regs.R_PC] + pc_offset))
-			update_flags(r0)
+			PCoffset9 := signExtend(instr&0x1FF, 9)
+			regs.REG[r0] = mem_read(mem_read(regs.REG[regs.R_PC] + PCoffset9))
+			updateFlags(r0)
 			break
 		case OPS.LDR:
+			r0 := (instr >> 9) & 0x7
+			r1 := (instr >> 6) & 0x7
+			PCoffset6 := signExtend(instr&0x3F, 6)
+			regs.REG[r0] = mem_read(regs.REG[r1] + PCoffset6)
+			updateFlags(r0)
 			break
 		case OPS.LEA:
+			r0 := (instr >> 9) & 0x7
+			PCoffset9 := signExtend(instr&0x3F, 6)
+			regs.REG[r0] = regs.REG[regs.R_PC] + PCoffset9
+			updateFlags(r0)
 			break
 		case OPS.ST:
+			sr := (instr >> 9) & 0x7
+			PCoffset9 := signExtend(instr&0x3F, 6)
+			mem_write(regs.REG[regs.R_PC]+PCoffset9, regs.REG[sr])
 			break
 		case OPS.STI:
+			sr := (instr >> 9) & 0x7
+			PCoffset9 := signExtend(instr&0x3F, 6)
+			mem_write(mem_read(regs.REG[regs.R_PC] + PCoffset9), regs.REG[sr])
+			break
 		case OPS.STR:
+			sr := (instr >> 9) & 0x7
+			r1 := (instr >> 6) & 0x7
+			PCoffset6 := signExtend(instr&0x3F, 6)
+			mem_write(regs.REG[r1] + PCoffset6, regs.REG[sr])
+
+			break
 		case OPS.TRAP:
+
+			break
 		case OPS.RES:
 			break
 		case OPS.RTI:
@@ -103,7 +170,7 @@ func main() {
 
 }
 
-func sign_extend(x uint16, bit_count int) uint16 {
+func signExtend(x uint16, bit_count int) uint16 {
 
 	if (x>>(bit_count-1))&1 == 1 {
 		return x | (0xFFFF << bit_count)
@@ -112,7 +179,7 @@ func sign_extend(x uint16, bit_count int) uint16 {
 
 }
 
-func update_flags(r uint16) {
+func updateFlags(r uint16) {
 	if regs.REG[r] == 0 {
 		regs.REG[regs.R_COND] = fls.FL_ZRO
 
